a313b15cd8bec8fd4502c7bde2c86f6379fc157d
Joshua Daniel <joshua_dup@hotmail.com>
2023-12-12 10:04:47 -0500

    creo los componentes y utilizo props para la interactividad del TodoItem

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './platzi.webp';
-import './App.css';
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch";
+import { TodoList } from "./TodoList";
+import { TodoItem } from "./TodoItem";
+import { CreateTodoButton } from "./CreateTodoButton";
+import React from "react";
+import "./App.css";
+
+const defaultTodos = [
+  { text: "Programar en React", completed: true },
+  { text: "Hacer ejercicio", completed: false },
+  { text: "Sacar a Zeus", completed: true },
+  { text: "Implementar el nuevo componente en el proyecto", completed: false },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <TodoCounter completed={23} total={31} />
+      <TodoSearch />
+
+      <TodoList>
+        {defaultTodos.map((todo) => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+          />
+        ))}
+      </TodoList>
+
+      <CreateTodoButton />
+    </React.Fragment>
   );
 }
 
---
+++ src/TodoSearch.js
@@ -0,0 +1,5 @@
+function TodoSearch() {
+  return <input type="text" placeholder="Ingresa un dato" />;
+}
+
+export { TodoSearch };
